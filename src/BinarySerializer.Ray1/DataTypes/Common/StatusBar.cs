51115f19e1ae6b7db74a0f55ee1fdd50ffe62826
RayCarrot <33812678+RayCarrot@users.noreply.github.com>
2022-06-08 15:26:06 +0200

    R2 Proto: Serialize status bar

--- src/BinarySerializer.Ray1/DataTypes/Common/StatusBar.cs
+++ src/BinarySerializer.Ray1/DataTypes/Common/StatusBar.cs
@@ -5,7 +5,7 @@
         /// <summary>
         /// The current number of lives, 0-99
         /// </summary>
-        public byte LivesCount { get; set; }
+        public short LivesCount { get; set; }
 
         public byte Byte_01 { get; set; } // Related to the graphics index for the lives count
         public byte LivesDigit0 { get; set; }
@@ -31,16 +31,31 @@
         /// <param name="s">The serializer object</param>
         public override void SerializeImpl(SerializerObject s)
         {
-            LivesCount = s.Serialize<byte>(LivesCount, name: nameof(LivesCount));
-            Byte_01 = s.Serialize<byte>(Byte_01, name: nameof(Byte_01));
-            LivesDigit0 = s.Serialize<byte>(LivesDigit0, name: nameof(LivesDigit0));
-            LivesDigit1 = s.Serialize<byte>(LivesDigit1, name: nameof(LivesDigit1));
-            Byte_04 = s.Serialize<byte>(Byte_04, name: nameof(Byte_04));
-            Byte_05 = s.Serialize<byte>(Byte_05, name: nameof(Byte_05));
-            TingsCount = s.Serialize<byte>(TingsCount, name: nameof(TingsCount));
-            TingsDigit0 = s.Serialize<byte>(TingsDigit0, name: nameof(TingsDigit0));
-            TingsDigit1 = s.Serialize<byte>(TingsDigit1, name: nameof(TingsDigit1));
-            MaxHealth = s.Serialize<byte>(MaxHealth, name: nameof(MaxHealth));
+            Ray1Settings settings = s.GetSettings<Ray1Settings>();
+
+            if (settings.EngineVersion == Ray1EngineVersion.R2_PS1)
+            {
+                LivesCount = s.Serialize<short>(LivesCount, name: nameof(LivesCount));
+                LivesDigit0 = s.Serialize<byte>(LivesDigit0, name: nameof(LivesDigit0));
+                LivesDigit1 = s.Serialize<byte>(LivesDigit1, name: nameof(LivesDigit1));
+                MaxHealth = s.Serialize<byte>(MaxHealth, name: nameof(MaxHealth));
+                TingsCount = s.Serialize<byte>(TingsCount, name: nameof(TingsCount));
+                TingsDigit0 = s.Serialize<byte>(TingsDigit0, name: nameof(TingsDigit0));
+                TingsDigit1 = s.Serialize<byte>(TingsDigit1, name: nameof(TingsDigit1));
+            }
+            else
+            {
+                LivesCount = s.Serialize<byte>((byte)LivesCount, name: nameof(LivesCount));
+                Byte_01 = s.Serialize<byte>(Byte_01, name: nameof(Byte_01));
+                LivesDigit0 = s.Serialize<byte>(LivesDigit0, name: nameof(LivesDigit0));
+                LivesDigit1 = s.Serialize<byte>(LivesDigit1, name: nameof(LivesDigit1));
+                Byte_04 = s.Serialize<byte>(Byte_04, name: nameof(Byte_04));
+                Byte_05 = s.Serialize<byte>(Byte_05, name: nameof(Byte_05));
+                TingsCount = s.Serialize<byte>(TingsCount, name: nameof(TingsCount));
+                TingsDigit0 = s.Serialize<byte>(TingsDigit0, name: nameof(TingsDigit0));
+                TingsDigit1 = s.Serialize<byte>(TingsDigit1, name: nameof(TingsDigit1));
+                MaxHealth = s.Serialize<byte>(MaxHealth, name: nameof(MaxHealth));
+            }
         }
     }
 }
