f8a16e37ee89ecf0729d97fadaaaf1de72d89e87
RayCarrot <33812678+RayCarrot@users.noreply.github.com>
2022-06-15 10:30:01 +0200

    Serialize active objects table

---
+++ src/BinarySerializer.Ray1/DataTypes/Common/ActiveTable.cs
@@ -0,0 +1,21 @@
+﻿namespace BinarySerializer.Ray1
+{
+    public class ActiveTable : BinarySerializable
+    {
+        public short[] ActiveObjects { get; set; }
+        public short ActiveObjectsCount { get; set; }
+        public short[] Unknown { get; set; } // Appears unused?
+
+        public override void SerializeImpl(SerializerObject s)
+        {
+            var settings = s.GetSettings<Ray1Settings>();
+            bool isGBA = settings.EngineBranch == Ray1EngineBranch.GBA;
+
+            ActiveObjects = s.SerializeArray<short>(ActiveObjects, isGBA ? 200 : 100, name: nameof(ActiveObjects));
+            ActiveObjectsCount = s.Serialize<short>(ActiveObjectsCount, name: nameof(ActiveObjectsCount));
+
+            if (!isGBA)
+                Unknown = s.SerializeArray<short>(Unknown, 11, name: nameof(Unknown));
+        }
+    }
+}
