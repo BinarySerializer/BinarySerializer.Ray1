f8ba1753d6a772f21601d13001f8682786266221
RayCarrot <33812678+RayCarrot@users.noreply.github.com>
2023-02-17 16:50:35 +0100

    PS1: Fix reading fond sprites

--- src/BinarySerializer.Ray1/DataTypes/PS1/VIG/PS1_FondPack.cs
+++ src/BinarySerializer.Ray1/DataTypes/PS1/VIG/PS1_FondPack.cs
@@ -15,7 +15,10 @@
 
             // Serialize files
             SerializeFile(s, 0, _ => Fond = s.SerializeObject<PS1_Fond>(Fond, name: nameof(Fond)));
-            SerializeFile(s, 1, _ => SpriteData = s.SerializeObject<PS1_FondSpriteData>(SpriteData, name: nameof(SpriteData)));
+
+            // TODO: Game ignores this data if this condition is not met, but the data still exists. What is it then?
+            if (Fond.Type is 6 or 7 or 8 or 9 or 10 or 11 or 12)
+                SerializeFile(s, 1, _ => SpriteData = s.SerializeObject<PS1_FondSpriteData>(SpriteData, name: nameof(SpriteData)));
 
             // Go to the end of the pack
             s.Goto(Offset + PackSize);
--- src/BinarySerializer.Ray1/DataTypes/PS1/VIG/PS1_FondSpriteData.cs
+++ src/BinarySerializer.Ray1/DataTypes/PS1/VIG/PS1_FondSpriteData.cs
@@ -6,8 +6,9 @@ namespace BinarySerializer.Ray1
     {
         public byte SpritesCount { get; set; }
         public byte UnkCount1 { get; set; }
-        public byte PalettesCount { get; set; }
         public byte UnkCount2 { get; set; }
+        public byte PalettesCount { get; set; }
+        public byte[] Bytes_05 { get; set; } // Seems to be unused by the game
         
         public UnknownData[] UnkData1 { get; set; }
         public UnknownData[] UnkData2 { get; set; }
@@ -17,12 +18,16 @@ namespace BinarySerializer.Ray1
         public override void SerializeImpl(SerializerObject s)
         {
             SpritesCount = s.Serialize<byte>(SpritesCount, name: nameof(SpritesCount));
+            s.SerializePadding(1, logIfNotNull: true);
             UnkCount1 = s.Serialize<byte>(UnkCount1, name: nameof(UnkCount1));
-            PalettesCount = s.Serialize<byte>(PalettesCount, name: nameof(PalettesCount));
             UnkCount2 = s.Serialize<byte>(UnkCount2, name: nameof(UnkCount2));
+            PalettesCount = s.Serialize<byte>(PalettesCount, name: nameof(PalettesCount));
+            Bytes_05 = s.SerializeArray<byte>(Bytes_05, 3, name: nameof(Bytes_05));
 
             UnkData1 = s.SerializeObjectArray<UnknownData>(UnkData1, UnkCount1, name: nameof(UnkData1));
             UnkData2 = s.SerializeObjectArray<UnknownData>(UnkData2, UnkCount2, name: nameof(UnkData2));
+
+            s.Align();
 
             Palettes = s.SerializeObjectArray<Clut>(Palettes, PalettesCount, name: nameof(Palettes));
         }
