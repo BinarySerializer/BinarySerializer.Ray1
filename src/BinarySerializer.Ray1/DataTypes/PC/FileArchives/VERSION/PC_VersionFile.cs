c0b891a4ca3f8acc6dc014bb9b769be3d1b5f526
RayCarrot <33812678+RayCarrot@users.noreply.github.com>
2023-05-23 09:32:08 +0200

    PC_VersionMemoryInfo -> PC_MemorySizes

--- src/BinarySerializer.Ray1/DataTypes/PC/FileArchives/VERSION/PC_VersionFile.cs
+++ src/BinarySerializer.Ray1/DataTypes/PC/FileArchives/VERSION/PC_VersionFile.cs
@@ -9,26 +9,24 @@
 
         public string[] VersionModes { get; set; }
 
-        public PC_VersionMemoryInfo[] VersionMemoryInfos { get; set; }
+        public PC_MemorySizes[] MemorySizes { get; set; }
 
         public string DefaultPrimaryHeader { get; set; }
         public string DefaultSecondaryHeader { get; set; }
         public ushort UnkHeaderValue { get; set; }
 
-        /// <summary>
-        /// Handles the data serialization
-        /// </summary>
-        /// <param name="s">The serializer object</param>
         public override void SerializeImpl(SerializerObject s)
         {
-            var settings = s.GetRequiredSettings<Ray1Settings>();
+            Ray1Settings settings = s.GetRequiredSettings<Ray1Settings>();
 
             VersionsCount = s.Serialize<byte>(VersionsCount, name: nameof(VersionsCount));
             RuntimeCurrentVersion = s.Serialize<byte>(RuntimeCurrentVersion, name: nameof(RuntimeCurrentVersion));
 
+            // TODO: Read as linear file
+
             s.DoAt(Offset + 0x02, () => VersionCodes = s.SerializeStringArray(VersionCodes, VersionsCount, 5, name: nameof(VersionCodes)));
             s.DoAt(Offset + 0x52, () => VersionModes = s.SerializeStringArray(VersionModes, VersionsCount, 20, name: nameof(VersionModes)));
-            s.DoAt(Offset + 0x192, () => VersionMemoryInfos = s.SerializeObjectArray<PC_VersionMemoryInfo>(VersionMemoryInfos, VersionsCount, name: nameof(VersionMemoryInfos)));
+            s.DoAt(Offset + 0x192, () => MemorySizes = s.SerializeObjectArray<PC_MemorySizes>(MemorySizes, VersionsCount, name: nameof(MemorySizes)));
 
             s.DoAt(Offset + (settings.EngineVersion == Ray1EngineVersion.PC_Kit || settings.EngineVersion == Ray1EngineVersion.PC_Fan ? 0x392 : 0x312), () =>
             {
