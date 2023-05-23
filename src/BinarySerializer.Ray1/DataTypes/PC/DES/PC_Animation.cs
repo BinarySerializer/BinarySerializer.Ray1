d128933cc6f6022b99bf2b2b8444daa5f30befb3
RayCarrot <33812678+RayCarrot@users.noreply.github.com>
2023-05-23 09:25:08 +0200

    Use DataLengthProcessor for PC animations

--- src/BinarySerializer.Ray1/DataTypes/PC/DES/PC_Animation.cs
+++ src/BinarySerializer.Ray1/DataTypes/PC/DES/PC_Animation.cs
@@ -9,11 +9,10 @@
         public byte SpeedXValue { get; set; } // Value used for setting horizontal obj speed
         public ushort FramesCount { get; set; }
 
-        public uint FramesOffset { get; set; }
-        public ushort AnimationDataLength { get; set; }
+        public uint FramesPointer { get; set; } // Game just checks so this is not 0xFFFFFFFF
 
         public AnimationLayer[] Layers { get; set; }
-        public AnimationFrame DefaultFrame { get; set; }
+        public AnimationFrame DefaultFrame { get; set; } // Unused by game
         public AnimationFrame[] Frames { get; set; }
 
         public override void SerializeImpl(SerializerObject s) 
@@ -25,11 +24,16 @@
             });
             FramesCount = s.Serialize<ushort>(FramesCount, name: nameof(FramesCount));
 
-            FramesOffset = s.Serialize<uint>(FramesOffset, name: nameof(FramesOffset));
-            AnimationDataLength = s.Serialize<ushort>(AnimationDataLength, name: nameof(AnimationDataLength));
+            FramesPointer = s.Serialize<uint>(FramesPointer, name: nameof(FramesPointer));
 
-            Layers = s.SerializeObjectArray<AnimationLayer>(Layers, LayersCount * FramesCount, name: nameof(Layers));
-            DefaultFrame = s.SerializeObject<AnimationFrame>(DefaultFrame, name: nameof(DefaultFrame));
+            s.DoProcessed(new DataLengthProcessor(), p =>
+            {
+                p.Serialize<ushort>(s, "FramesOffset");
+
+                Layers = s.SerializeObjectArray<AnimationLayer>(Layers, LayersCount * FramesCount, name: nameof(Layers));
+                DefaultFrame = s.SerializeObject<AnimationFrame>(DefaultFrame, name: nameof(DefaultFrame));
+            });
+
             Frames = s.SerializeObjectArray<AnimationFrame>(Frames, FramesCount, name: nameof(Frames));
         }
     }
