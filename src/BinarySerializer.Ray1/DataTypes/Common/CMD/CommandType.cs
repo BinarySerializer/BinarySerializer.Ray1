10c30007fafecac8e897f0424f6eae62e81ec3bc
RayCarrot <33812678+RayCarrot@users.noreply.github.com>
2023-05-26 19:53:07 +0200

    Improve command type comments

--- src/BinarySerializer.Ray1/DataTypes/Common/CMD/CommandType.cs
+++ src/BinarySerializer.Ray1/DataTypes/Common/CMD/CommandType.cs
@@ -9,16 +9,33 @@
     /// </summary>
     public enum CommandType : byte
     {
+        /// <summary>
+        /// Self-handled
+        /// </summary>
         GO_LEFT = 0x0,
+
+        /// <summary>
+        /// Self-handled
+        /// </summary>
         GO_RIGHT = 0x1,
 
+        /// <summary>
+        /// Self-handled
+        /// </summary>
         GO_WAIT = 0x2,
 
+        /// <summary>
+        /// Self-handled
+        /// </summary>
         GO_UP = 0x3,
+
+        /// <summary>
+        /// Self-handled
+        /// </summary>
         GO_DOWN = 0x4,
 
         /// <summary>
-        /// Set SubEtat to {arg1}
+        /// Set sub etat to {arg1}
         /// </summary>
         GO_SUBSTATE = 0x5,
 
@@ -27,15 +44,18 @@
         /// </summary>
         GO_SKIP = 0x6,
 
+        /// <summary>
+        /// Unused, does nothing
+        /// </summary>
         GO_ADD = 0x7,
 
         /// <summary>
-        /// Set Etat to {arg1}
+        /// Set main etat to {arg1}
         /// </summary>
         GO_STATE = 0x8,
 
         /// <summary>
-        /// Saves the current position and prepares a loop which loops with {arg1} being the amount of times to loop
+        /// Saves the current command position and prepares a loop which loops {arg1} times
         /// </summary>
         GO_PREPARELOOP = 0x9,
 
@@ -45,7 +65,7 @@
         GO_DOLOOP = 0xA,
         
         /// <summary>
-        /// Sets label {arg1}
+        /// Defines label {arg1}
         /// </summary>
         GO_LABEL = 0xB,
 
@@ -55,99 +75,102 @@
         GO_GOTO = 0xC,
 
         /// <summary>
-        /// Saves the current position and starts executing from label {arg1} until GO_RETURN
+        /// Saves the current command position and starts executing from label {arg1} until <see cref="GO_RETURN"/>
         /// </summary>
         GO_GOSUB = 0xD,
 
         /// <summary>
-        /// Returns from a GOSUB command execution
+        /// Returns from a <see cref="GO_GOSUB"/> command execution
         /// </summary>
         GO_RETURN = 0xE,
 
         /// <summary>
-        /// Skips to label {arg1} if true
+        /// Skips to label {arg1} if TEST flag is true
         /// </summary>
         GO_BRANCHTRUE = 0xF,
 
         /// <summary>
-        /// Skips to label {arg1} if false
+        /// Skips to label {arg1} if TEST flag is false
         /// </summary>
         GO_BRANCHFALSE = 0x10,
 
         /// <summary>
-        /// Sets the TEST flag depending on {arg1}. If {arg1} <= 4 then {arg2} is used in the validation, otherwise only one argument is expected.
+        /// Sets the TEST flag depending on {arg1}. If {arg1} is less than 5 then {arg2} is used in the validation, otherwise only one argument is expected.
         /// </summary>
         GO_TEST = 0x11,
 
         /// <summary>
-        /// Sets the TEST flag to {arg1}, treated as a boolean
+        /// Sets the TEST flag to {arg1}
         /// </summary>
         GO_SETTEST = 0x12,
         
         /// <summary>
-        /// Waits {arg1} seconds
+        /// Waits for the animation to play {arg1} times. The command gets set to <see cref="GO_WAIT"/> while waiting.
         /// </summary>
         GO_WAITSTATE = 0x13,
-        
+
         /// <summary>
-        /// Moves for {arg1} seconds with speed X {arg2} and Y {arg3}
+        /// Self-handled. Usually {arg2} is speed x and {arg3} is speed y
         /// </summary>
         GO_SPEED = 0x14,
         
         /// <summary>
-        /// Sets the X position to {arg1}{arg2}
+        /// Sets the x position to {arg1}{arg2}
         /// </summary>
         GO_X = 0x15,
 
         /// <summary>
-        /// Sets the Y position to {arg1}{arg2}
+        /// Sets the y position to {arg1}{arg2}
         /// </summary>
         GO_Y = 0x16,
 
         /// <summary>
-        /// Skips to LabelOffsets[{arg1}]
+        /// Skips to label offset index {arg1}
         /// </summary>
         RESERVED_GO_SKIP = 0x17,
 
         /// <summary>
-        /// Same as RESERVED_GO_SKIP
+        /// Same as <see cref="RESERVED_GO_SKIP"/>
         /// </summary>
         RESERVED_GO_GOTO = 0x18,
 
         /// <summary>
-        /// Saves the current position and starts executing from LabelOffsets[{arg1}] until GO_RETURN
+        /// Saves the current command position and starts executing from label offset index {arg1} until <see cref="GO_RETURN"/>
         /// </summary>
         RESERVED_GO_GOSUB = 0x19,
 
         /// <summary>
-        /// Skips to LabelOffsets[{arg1}] if true
+        /// Skips to label offset index {arg1} if TEST is true
         /// </summary>
         RESERVED_GO_GOTOT = 0x1A,
 
         /// <summary>
-        /// Skips to LabelOffsets[{arg1}] if false
+        /// Skips to label offset index {arg1} if TEST is false
         /// </summary>
         RESERVED_GO_GOTOF = 0x1B,
 
         /// <summary>
-        /// Same as RESERVED_GO_GOTOT
+        /// Same as <see cref="RESERVED_GO_GOTOT"/>
         /// </summary>
         RESERVED_GO_SKIPT = 0x1C,
 
         /// <summary>
-        /// Same as RESERVED_GO_GOTOF
+        /// Same as <see cref="RESERVED_GO_GOTOF"/>
         /// </summary>
         RESERVED_GO_SKIPF = 0x1D,
 
+        /// <summary>
+        /// Self-handled
+        /// </summary>
         GO_NOP = 0x1E,
 
         /// <summary>
-        /// Skips {arg1} commands if true
+        /// Skips {arg1} commands if TEST is true
         /// </summary>
         GO_SKIPTRUE = 0x1F,
 
         /// <summary>
-        /// Skips {arg1} commands if false
+        /// Skips {arg1} commands if TEST is false
         /// </summary>
         GO_SKIPFALSE = 0x20,
 
@@ -156,7 +179,9 @@
         /// </summary>
         INVALID_CMD = 0x21,
 
-        // Used in the PS1 demos
+        /// <summary>
+        /// Same as <see cref="INVALID_CMD"/>. Used in the earlier PS1 demos.
+        /// </summary>
         INVALID_CMD_DEMO = 0x42
     }
 }
