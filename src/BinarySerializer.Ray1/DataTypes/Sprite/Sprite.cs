342dfa4513701b9c98034526f0ef5650ae800ab8
RayCarrot <33812678+RayCarrot@users.noreply.github.com>
2026-04-08 13:17:42 +0200

    Cleaned up Sprite class

--- src/BinarySerializer.Ray1/DataTypes/Sprite/Sprite.cs
+++ src/BinarySerializer.Ray1/DataTypes/Sprite/Sprite.cs
@@ -2,64 +2,46 @@
 
 namespace BinarySerializer.Ray1
 {
-    /// <summary>
-    /// A sprite
-    /// </summary>
     public class Sprite : BinarySerializable
     {
-        /// <summary>
-        /// The offset in the image buffer where this sprite is located
-        /// </summary>
+        // The offset in the image buffer where this sprite is located
         public int ImageBufferOffset { get; set; }
 
-        /// <summary>
-        /// The image id. 0 means it's invalid.
-        /// </summary>
+        // The ID. Treated as a signed 16-bit value in most versions where 0, or sometimes less
+        // than 0, is invalid.
         public ushort? Id { get; set; }
-        
-        /// <summary>
-        /// The outer image width. This is used when drawing the sprite.
-        /// </summary>
-        public short Width { get; set; }
 
-        /// <summary>
-        /// The outer image height. This is used when drawing the sprite.
-        /// </summary>
+        // The full image size, used for drawing
+        public short Width { get; set; }
         public short Height { get; set; }
 
-        /// <summary>
-        /// The inner image width. This is used to calculate the sprite size in-game.
-        /// </summary>
+        // The inner image size and position, excluding any transparent padding. This is used
+        // to calculate the sprite size in-game.
         public byte SpriteWidth { get; set; }
-
-        /// <summary>
-        /// The inner image height. This is used to calculate the sprite size in-game.
-        /// </summary>
         public byte SpriteHeight { get; set; }
-
         public byte SpriteXPosition { get; set; }
         public byte SpriteYPosition { get; set; }
 
-        /// <summary>
-        /// The sprite image depth. Only 4-bit and 8-bit modes are used.
-        /// </summary>
+        // The image depth. Only 4-bit and 8-bit modes are used.
         public SpriteDepth Depth { get; set; }
 
-        /// <summary>
-        /// The sub-palette index to use in a 256-color palette. This is only
-        /// used for 4-bit sprites in some versions when loading directly from
-        /// palettes rather than from vram.
-        /// </summary>
+        // The sub-palette index to use in a 256-color palette. This is only used for 4-bit
+        // sprites in some versions when loading directly from palettes rather than from VRAM.
         public byte SubPaletteIndex { get; set; }
 
-        public byte Flags { get; set; }
+        // Horizontal flip (on PC it's a flag instead)
+        public bool FlipX { get; set; }
 
-        // Unknown values. Might be different for different versions.
-        public byte Unknown1 { get; set; }
-        public byte Unknown2 { get; set; }
-        public byte Unknown3 { get; set; }
+        // Same as width on Saturn. On PS1 it's used to indicate if the sprite has been processed (0 == no, 0xFF == yes).
+        public byte ScanlineWidth { get; set; }
 
-        // PS1
+        // Flags, used a bit different based on version
+        public SpriteFlags Flags { get; set; }
+
+        // Unknown unused values
+        public byte UnusedValue { get; set; }
+
+        // PS1 VRAM data
         public CBA Clut { get; set; }
         public TSB TexturePage { get; set; }
         public byte ImageOffsetInPageX { get; set; }
@@ -117,9 +99,8 @@ namespace BinarySerializer.Ray1
                     JAG_Index = b.SerializeBits<short>(JAG_Index, 7, name: nameof(JAG_Index));
 
                     // TODO: Parse the flags. They probably match the Jaguar Object flags. Usually
-                    // this value is set to 4, which would be the transparency flag, which makes
-                    // sense. Unsure about the rest though. It seems the horizontal flip flag is
-                    // copied to the multi-platform flags in Designer, but why? It's unused there.
+                    //       this value is set to 4, which would be the transparency flag, which makes
+                    //       sense. Unsure about the rest though since there are only 4 flags...
                     JAG_Flags = b.SerializeBits<int>(JAG_Flags, 6, name: nameof(JAG_Flags));
 
                     JAG_Width = b.SerializeBits<short>(JAG_Width, 13, name: nameof(JAG_Width));
@@ -150,15 +131,16 @@ namespace BinarySerializer.Ray1
                     SpriteXPosition = b.SerializeBits<byte>(SpriteXPosition, 4, name: nameof(SpriteXPosition));
                     SpriteYPosition = b.SerializeBits<byte>(SpriteYPosition, 4, name: nameof(SpriteYPosition));
                 });
-                // Unused
                 s.DoBits<byte>(b =>
                 {
+                    // Unused
                     Depth = b.SerializeBits<SpriteDepth>(Depth, 4, name: nameof(Depth));
+
+                    // It gets read when rendering, but appears to remain unused
                     SubPaletteIndex = b.SerializeBits<byte>(SubPaletteIndex, 4, name: nameof(SubPaletteIndex));
                 });
-                // TODO: Parse flags. The reflect flag from Jaguar seems to be copied in here? The
-                // game doesn't support reflected sprites though, so that doesn't make much sense.
-                Flags = s.Serialize<byte>(Flags, name: nameof(Flags));
+
+                Flags = s.Serialize<SpriteFlags>(Flags, name: nameof(Flags));
             }
             else if (settings.EngineBranch == Ray1EngineBranch.PS1)
             {
@@ -173,7 +155,9 @@ namespace BinarySerializer.Ray1
                         SpriteXPosition = b.SerializeBits<byte>(SpriteXPosition, 4, name: nameof(SpriteXPosition));
                         SpriteYPosition = b.SerializeBits<byte>(SpriteYPosition, 4, name: nameof(SpriteYPosition));
                     });
-                    Unknown1 = s.Serialize<byte>(Unknown1, name: nameof(Unknown1));
+                    Flags = s.Serialize<SpriteFlags>(Flags, name: nameof(Flags));
+
+                    // PS1 VRAM data
                     ImageOffsetInPageX = s.Serialize<byte>(ImageOffsetInPageX, name: nameof(ImageOffsetInPageX));
                     ImageOffsetInPageY = s.Serialize<byte>(ImageOffsetInPageY, name: nameof(ImageOffsetInPageY));
                     Clut = s.SerializeObject<CBA>(Clut, name: nameof(Clut));
@@ -184,21 +168,38 @@ namespace BinarySerializer.Ray1
                          settings.EngineVersion == Ray1EngineVersion.PS1_JPDemoVol6 ||
                          settings.EngineVersion == Ray1EngineVersion.Saturn)
                 {
+                    // Only used on PS1 JP vol. 3 demo and Saturn
                     ImageBufferOffset = s.Serialize<int>(ImageBufferOffset, name: nameof(ImageBufferOffset));
+
+                    // The game doesn't treat this as a 12-bit value, but rather as a signed 16-bit value it checks for being greater
+                    // than 0, or sometimes not equal to 0. But it appears that the invalid values are all set to 0xFFF since the
+                    // original Jaguar value uses 12 bits.
                     s.DoBits<ushort>(b =>
                     {
                         Id = b.SerializeNullableBits<ushort>(Id, 12, name: nameof(Id));
                         b.SerializePadding(4, logIfNotNull: true);
                     });
+
+                    // Only used on PS1 JP vol. 3 demo and Saturn 
                     Depth = s.Serialize<SpriteDepth>(Depth, name: nameof(Depth));
+
                     Width = s.Serialize<short>(Width, name: nameof(Width));
                     Height = s.Serialize<short>(Height, name: nameof(Height));
 
+                    // Only used on PS1 JP vol. 3 demo and Saturn
                     SubPaletteIndex = s.Serialize<byte>(SubPaletteIndex, name: nameof(SubPaletteIndex));
-                    Unknown1 = s.Serialize<byte>(Unknown1, name: nameof(Unknown1));
-                    Unknown2 = s.Serialize<byte>(Unknown2, name: nameof(Unknown2));
-                    Unknown3 = s.Serialize<byte>(Unknown3, name: nameof(Unknown3));
 
+                    // Only used on Saturn
+                    FlipX = s.Serialize<bool>(FlipX, name: nameof(FlipX));
+
+                    // Unused for all versions
+                    UnusedValue = s.Serialize<byte>(UnusedValue, name: nameof(UnusedValue));
+
+                    // On Saturn it's always the same as the width. On PS1 it is used to indicate if the sprite
+                    // has been processed (0 == no, 0xFF == yes).
+                    ScanlineWidth = s.Serialize<byte>(ScanlineWidth, name: nameof(ScanlineWidth));
+
+                    // PS1 VRAM data
                     if (settings.EngineVersion != Ray1EngineVersion.Saturn)
                     {
                         s.DoWithDefaults(new SerializerDefaults()
@@ -216,12 +217,17 @@ namespace BinarySerializer.Ray1
                         });
                     }
 
+                    // The width and height are used as the sprite size here
                     SpriteWidth = (byte)Width;
                     SpriteHeight = (byte)Height;
+                    SpriteXPosition = 0;
+                    SpriteYPosition = 0;
                 }
                 else
                 {
+                    // Unused
                     ImageBufferOffset = s.Serialize<int>(ImageBufferOffset, name: nameof(ImageBufferOffset));
+                    
                     Id = s.SerializeNullable<byte>((byte?)Id, name: nameof(Id));
                     Width = s.Serialize<byte>((byte)Width, name: nameof(Width));
                     Height = s.Serialize<byte>((byte)Height, name: nameof(Height));
@@ -232,12 +238,16 @@ namespace BinarySerializer.Ray1
                         SpriteXPosition = b.SerializeBits<byte>(SpriteXPosition, 4, name: nameof(SpriteXPosition));
                         SpriteYPosition = b.SerializeBits<byte>(SpriteYPosition, 4, name: nameof(SpriteYPosition));
                     });
+                    
+                    // Unused (depth doesn't always match here since it's a leftover - use the texture page info instead!)
                     s.DoBits<byte>(b =>
                     {
                         Depth = b.SerializeBits<SpriteDepth>(Depth, 4, name: nameof(Depth));
                         SubPaletteIndex = b.SerializeBits<byte>(SubPaletteIndex, 4, name: nameof(SubPaletteIndex));
                     });
-                    Unknown1 = s.Serialize<byte>(Unknown1, name: nameof(Unknown1));
+                    UnusedValue = s.Serialize<byte>(UnusedValue, name: nameof(UnusedValue));
+
+                    // PS1 VRAM data
                     Clut = s.SerializeObject<CBA>(Clut, name: nameof(Clut));
                     TexturePage = s.SerializeObject<TSB>(TexturePage, name: nameof(TexturePage));
                     ImageOffsetInPageX = s.Serialize<byte>(ImageOffsetInPageX, name: nameof(ImageOffsetInPageX));
---
+++ src/BinarySerializer.Ray1/DataTypes/Sprite/SpriteFlags.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace BinarySerializer.Ray1
+{
+    [Flags]
+    public enum SpriteFlags : byte
+    {
+        None = 0,
+        Flag_0 = 1 << 0,
+        SemiTransparent = 1 << 1, // Only used in the Rayman 2 2D prototype
+        FlipX = 1 << 2, // Only used in the PC versions
+        Flag_3 = 1 << 3,
+        Flag_4 = 1 << 4,
+        Flag_5 = 1 << 5,
+        Flag_6 = 1 << 6,
+        Flag_7 = 1 << 7,
+    }
+}
